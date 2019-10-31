b2dbdb031c35f6454dd2abcb6386a4fe4b6682cf
aristaadjam <57220087+aristaadjam@users.noreply.github.com>
2019-10-31 21:17:40 +0900

    tugas2.

---
+++ pgame16074/code-tugas2.py
@@ -0,0 +1,5 @@
+print ("Hello, my friends") #Halo, teman-teman
+print ("my name is arista adjam") #nama saya arista adjam
+#baris ini tidak akan dieksekusi
+print ("computing friends...") #baris ini dieksekusi
+#Baris ini juga merupakan Baris Komentar 
