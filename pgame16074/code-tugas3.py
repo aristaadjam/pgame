09c66831a40b83615568637ea1e108723f1141f4
aristaadjam <57220087+aristaadjam@users.noreply.github.com>
2019-10-31 21:20:46 +0900

    tugas3

---
+++ pgame16074/code-tugas3.py
@@ -0,0 +1,10 @@
+# Tugas 3. Praktik Git untuk MK Pemrograman Game
+
+nama = "Nama 		: arista adjam" 
+print (nama)
+npm = "NPM 		: 121055520116074" 
+print (npm)
+semester = "Semester 	: 7 (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
